4fa9c43eaac8fe02fabdfa3efd21a60164255049
David Mee <dmee1@kpmg.com.au>
2023-08-02 21:17:18 +1000

    add visualisation

--- prototypes/plotly_go_map_example.py
+++ prototypes/plotly_go_map_example.py
@@ -3,11 +3,13 @@ import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 
-import data
+from data import data, shapes
+from visualisations import maps as vis_maps
 
-df_shapes = pd.read_csv(data.data.path / "shapes.txt")\
-    .pipe(data.shapes.restructure)\
-    .pipe(data.shapes.add_end_lat_lon)
+#%%
+df_shapes = pd.read_csv(data.path / "shapes.txt")\
+    .pipe(shapes.restructure)\
+    .pipe(shapes.add_end_lat_lon)
 
 df_shapes.sample(20)
 
@@ -32,35 +34,6 @@ print(list(sample_ids))
 #%%
 
 df_sample = df_shapes[df_shapes.shape_id.isin(nice_ids)]
-fig = go.Figure()
-
-for id, df_id in df_sample.groupby("shape_id"):
-    fig.add_trace(
-        go.Scattermapbox(
-            mode = "lines",
-            lon = df_id['lon'],
-            lat = df_id['lat'],
-            name=id
-        )
-    )
-
-fig.update_layout(
-    title_text = 'Example bus routes',
-    margin ={'l':0,'t':0,'b':0,'r':0},
-    mapbox = {
-        'center': {'lon': 151.206, 'lat': -33.88357},
-        'style': "carto-positron",
-        'zoom': 6
-    },
-    geo = dict(
-        scope = 'asia',
-        projection_type = 'azimuthal equal area',
-        showland = True,
-        landcolor = 'rgb(243, 243, 243)',
-        countrycolor = 'rgb(204, 204, 204)',
-    ),
-)
-
+fig = vis_maps.routemap(df_sample)
 fig.show()
 
-# %%
---
+++ tests/test_map.py
@@ -0,0 +1,19 @@
+from pythontraining.visualisations import maps
+import pandas as pd
+from plotly.graph_objects import Figure
+
+
+def test_map():
+    # Test that a plot is produced
+    df = pd.DataFrame({
+        "shape_id":["2-CCN-sj2-31.41.H","2-CCN-sj2-31.41.H","2-CCN-sj2-31.41.H","2-CCN-sj2-31.41.H","2-CCN-sj2-31.41.H"],
+        "lat":[-33.883672,-33.884792,-33.885092,-33.8853,-33.885368],
+        "lon":[151.20648,151.2056,151.205312,151.20504,151.204912],
+        "shape_pt_sequence":[1,2,3,4,5],
+        "shape_dist_traveled":[0.0,1424.3595051812,1840.2248680091,2182.6398209536,2327.5811884096],
+        "end_lat":[-33.884792,-33.885092,-33.8853,-33.885368,-33.885584],
+        "end_lon":[151.2056,151.205312,151.20504,151.204912,151.20456]
+    }, index=["487515", "487516", "487517", "487518", "487519"]
+    )
+    fig = maps.routemap(df)
+    assert(isinstance(fig, Figure))
