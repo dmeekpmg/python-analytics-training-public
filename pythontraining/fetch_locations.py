4cc4ea1d5b04957e40c1cc53edb28fa475f50fd9
David Mee <dmee1@kpmg.com.au>
2023-09-15 11:10:21 +1000

    bugfix generator

--- pythontraining/fetch_locations.py
+++ pythontraining/fetch_locations.py
@@ -25,24 +25,31 @@ def generate_fetch_times(start_time=datetime.now(), wait_time=60, max_iters=60):
     """
     next_time = start_time
     max_time = start_time + timedelta(0, max_iters * wait_time)
-    while next_time < max_time:
+    print(f"Location fetch scheduled to start at {start_time} and end at {max_time}")
+    while next_time <= max_time:
         # If the time is in the past (e.g. we started the process late),
         # then jump to the next minute
-        while next_time < datetime.now():
+        while next_time <= datetime.now():
             next_time += timedelta(0, wait_time)
 
         yield next_time.timestamp()
         next_time += timedelta(0, wait_time)
+    
+    return None
 
 
 def schedule_fetch(scheduler:sched.scheduler, fetch_times:Sequence): 
     # schedule the next call first
-    scheduler.enterabs(
-        next(fetch_times), 
-        1, 
-        schedule_fetch, 
-        (scheduler,fetch_times)
-    )
+    try:
+        scheduler.enterabs(
+            next(fetch_times), 
+            1, 
+            schedule_fetch, 
+            (scheduler,fetch_times)
+        )
+    except StopIteration:
+        print("Last fetch has been reached")
+    
     try:
         realtime.fetch_and_upload_positions()
     except:
